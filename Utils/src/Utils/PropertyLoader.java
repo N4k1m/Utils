113bb6a773ab4e65d5f97101bf9ec3a5247e8b4e
Xavier Mawet <mawet.xavier@gmail.com>
2014-12-21 00:52:47 +0100

    Added class PropertyLoader

---
+++ Utils/src/Utils/PropertyLoader.java
@@ -0,0 +1,29 @@
+package Utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ *
+ * @author nakim
+ */
+public class PropertyLoader 
+{
+   public static Properties load(String filename) throws IOException, FileNotFoundException
+   {
+      Properties properties = new Properties();
+      FileInputStream input = new FileInputStream(filename);
+
+      try
+      {
+         properties.load(input);
+         return properties;
+      }
+      finally
+      {
+         input.close();
+      }
+   }
+}
